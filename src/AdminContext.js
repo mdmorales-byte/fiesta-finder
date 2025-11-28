5ad1febc97f4032beff119eb0413085007543f7e
Moral <mdmorales@gbox.adnu.edu.ph>
2025-11-28 21:33:48 +0800

    Revert "Add logging to AdminContext to trace festival creation and localStorage persistence on Vercel"
    This reverts commit 1365f4da386d3cd8276c2b54eb3192893bcd99a9.

--- src/AdminContext.js
+++ src/AdminContext.js
@@ -73,7 +73,6 @@ export const AdminProvider = ({ children }) => {
 
     setFestivals(prev => [...prev, newFestival]);
     return newFestival;
-    console.log('[AdminContext.addFestival] Festival created:', { name: newFestival.name, imageUrlsCount: newFestival.imageUrls.length });
   };
 
   const updateFestival = (festivalId, updatedData) => {
